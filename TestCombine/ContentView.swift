20af79fbb35ace9bcc0790ecc420184692745f20
=Ho Seong <kiz2010@naver.com>
2022-07-13 00:17:32 +0900

    future, just

--- TestCombine/ContentView.swift
+++ TestCombine/ContentView.swift
@@ -6,12 +6,51 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button("Future"){ callFuture() }
+            Button("Just"){ callJust() }
+        }
+        
     }
+}
+
+func callFuture(){
+    let cancellable = generateAsyncRandomNumberFromFuture()
+        .sink { number in
+            print("Got random number \(number).")
+        }
+}
+
+func generateAsyncRandomNumberFromFuture() -> Future<Int, Never>{
+    return Future() { promise in
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let number = Int.random(in: 1...10)
+            promise(Result.success(number))
+        }
+        
+    }
+}
+
+func callJust(){
+    
+//    let publisher = Just("aaa")
+    let publisher = ["aaa", "bbb", "ccc"].publisher
+    
+    let subscriber = publisher.sink { result in
+        switch result{
+        case .finished:
+            print("finished")
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    } receiveValue: { value in
+        print(value)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
