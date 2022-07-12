9d7bd79ea25379235068fb8de5d22416ff4e5c33
=Ho Seong <kiz2010@naver.com>
2022-07-12 23:18:35 +0900

    first commit

---
+++ TestCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestCombine
+//
+//  Created by 최호성 on 2022/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestCombine/TestCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCombineApp.swift
+//  TestCombine
+//
+//  Created by 최호성 on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct TestCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
